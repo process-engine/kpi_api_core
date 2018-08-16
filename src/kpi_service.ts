699eea8393502515d247c0a949b535a88463939d
ElRaptorus <christian.werner@5minds.de>
2018-08-16 16:57:58 +0200

    Add scaffolding

---
+++ src/kpi_service.ts
@@ -0,0 +1,49 @@
+import {ActiveToken, AverageFlowNodeRuntime, IKpiApiService} from '@process-engine/kpi_api_contracts';
+import {ILoggingRepository} from '@process-engine/logging_api_contracts';
+import {IFlowNodeInstanceRepository} from '@process-engine/process_engine_contracts';
+
+import {IIAMService, IIdentity} from '@essential-projects/iam_contracts';
+
+export class KpiApiService implements IKpiApiService {
+
+  private _loggingRepository: ILoggingRepository;
+  private _iamService: IIAMService;
+  private _flowNodeInstanceRepository: IFlowNodeInstanceRepository;
+
+  constructor(flowNodeInstanceRepository: IFlowNodeInstanceRepository, iamService: IIAMService, loggingRepository: ILoggingRepository) {
+    this._flowNodeInstanceRepository = flowNodeInstanceRepository;
+    this._iamService = iamService;
+    this._loggingRepository = loggingRepository;
+  }
+
+  private get flowNodeInstanceRepository(): IFlowNodeInstanceRepository {
+    return this._flowNodeInstanceRepository;
+  }
+
+  private get iamService(): IIAMService {
+    return this._iamService;
+  }
+
+  private get loggingRepository(): ILoggingRepository {
+    return this._loggingRepository;
+  }
+
+  public async getAverageRuntimeForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<AverageFlowNodeRuntime>> {
+    return Promise.resolve([]);
+  }
+
+  public async getAverageRuntimeForFlowNode(identity: IIdentity, processModelId: string, flowNodeId: string): Promise<AverageFlowNodeRuntime> {
+    return Promise.resolve({});
+
+  }
+
+  public async getActiveTokensForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<ActiveToken>> {
+    return Promise.resolve([]);
+
+  }
+
+  public async getActiveTokensForFlowNode(identity: IIdentity, flowNodeId: string): Promise<Array<ActiveToken>> {
+    return Promise.resolve([]);
+
+  }
+}
