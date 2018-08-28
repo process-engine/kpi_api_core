e05dd28f1a39bddada4432b7df3e93b6d07381d6
ElRaptorus <christian.werner@5minds.de>
2018-08-28 09:15:34 +0200

    Sync with poc

--- src/kpi_service.ts
+++ src/kpi_service.ts
@@ -1,8 +1,12 @@
 import {ActiveToken, FlowNodeRuntimeInformation, IKpiApiService} from '@process-engine/kpi_api_contracts';
 import {ILoggingRepository} from '@process-engine/logging_api_contracts';
-import {IFlowNodeInstanceRepository} from '@process-engine/process_engine_contracts';
+import {IFlowNodeInstanceRepository, Runtime} from '@process-engine/process_engine_contracts';
 
 import {IIAMService, IIdentity} from '@essential-projects/iam_contracts';
+
+type GroupedFlowNodeInstances = {
+  [flowNodeId: string]: Array<Runtime.Types.FlowNodeInstance>,
+};
 
 export class KpiApiService implements IKpiApiService {
 
@@ -29,23 +33,80 @@ export class KpiApiService implements IKpiApiService {
   }
 
   public async getRuntimeInformationForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<FlowNodeRuntimeInformation>> {
-    return Promise.resolve([]);
+
+    // TODO: Use logging repository
+    const flowNodeInstances: Array<Runtime.Types.FlowNodeInstance> = await this.flowNodeInstanceRepository.queryByProcessModel(processModelId);
+
+    const groupedFlowNodeInstances: GroupedFlowNodeInstances = this._groupFlowNodeInstancesByFlowNodeId(flowNodeInstances);
+
+    const groupKeys: Array<string> = Object.keys(groupedFlowNodeInstances);
+
+    const runtimeInformations: Array<FlowNodeRuntimeInformation> =
+      groupKeys.map((flowNodeId: string): FlowNodeRuntimeInformation => {
+        return this._createFlowNodeRuntimeInformation(flowNodeId, groupedFlowNodeInstances[flowNodeId]);
+      });
+
+    return Promise.resolve(runtimeInformations);
   }
 
   public async getRuntimeInformationForFlowNode(identity: IIdentity,
                                                 processModelId: string,
                                                 flowNodeId: string): Promise<FlowNodeRuntimeInformation> {
-    return Promise.resolve({});
 
+    // TODO: Use logging repository
+    const flowNodeInstances: Array<Runtime.Types.FlowNodeInstance> = await this.flowNodeInstanceRepository.queryByProcessModel(processModelId);
+
+    const matchingFlowNodeInstances: Array<Runtime.Types.FlowNodeInstance> =
+      flowNodeInstances.filter((flowNodeInstance: Runtime.Types.FlowNodeInstance): boolean => {
+
+        return flowNodeInstance.flowNodeId === flowNodeId;
+      });
+
+    const flowNodeRuntimeInformation: FlowNodeRuntimeInformation = this._createFlowNodeRuntimeInformation(flowNodeId, matchingFlowNodeInstances);
+
+    return flowNodeRuntimeInformation;
   }
 
   public async getActiveTokensForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<ActiveToken>> {
+
+    // TODO: Add flowNodeInstanceRepository.queryActiveTokensByProcessModelId
     return Promise.resolve([]);
 
   }
 
   public async getActiveTokensForFlowNode(identity: IIdentity, flowNodeId: string): Promise<Array<ActiveToken>> {
+    // TODO: Add flowNodeInstanceRepository.queryActiveTokensByFlowNodeId
     return Promise.resolve([]);
 
+  }
+
+  private _groupFlowNodeInstancesByFlowNodeId(flowNodeInstances: Array<Runtime.Types.FlowNodeInstance>): GroupedFlowNodeInstances {
+
+    const groupedFlowNodeInstances: GroupedFlowNodeInstances = {};
+
+    for (const flowNodeInstance of flowNodeInstances) {
+
+      const groupHasNoMatchingEntry: boolean = !groupedFlowNodeInstances[flowNodeInstance.flowNodeId];
+
+      if (!groupHasNoMatchingEntry) {
+        groupedFlowNodeInstances[flowNodeInstance.flowNodeId] = [];
+      }
+
+      groupedFlowNodeInstances[flowNodeInstance.flowNodeId].push(flowNodeInstance);
+    }
+
+    return groupedFlowNodeInstances;
+  }
+
+  private _createFlowNodeRuntimeInformation(flowNodeId: string,
+                                            flowNodeInstances: Array<Runtime.Types.FlowNodeInstance>,
+                                           ): FlowNodeRuntimeInformation {
+
+    // WIP
+    const runtimeInformation: FlowNodeRuntimeInformation = new FlowNodeRuntimeInformation();
+    runtimeInformation.flowNodeId = flowNodeId;
+    runtimeInformation.processModelId = flowNodeInstances[0].tokens[0].processModelId;
+
+    return runtimeInformation;
   }
 }
